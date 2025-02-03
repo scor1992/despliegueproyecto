cdc17da76176464e7cc0bf8b6b7149d4045de9aa
JOSE RICARDO BILCHES GONZALEZ <jbilches@uniguajira.edu.co>
2025-02-02 21:27:20 -0500

    corregido

--- routes/route.empleado.js
+++ routes/route.empleado.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router= express.Router();
 const empleadoCtrl=require ('../controller/empleadocontroller');
 
-router.post('/', empleadoCtrl.createEmpleado);
 router.get('/', empleadoCtrl.getEmpleados);
+router.post('/', empleadoCtrl.createEmpleado);
 router.get('/:id', empleadoCtrl.getUniqueEmpleado );
 router.put('/:id', empleadoCtrl.editEmpleado);
 router.delete('/:id', empleadoCtrl.deleteEmpleado)
