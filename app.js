2948c05444544274afc481040e4ae541ed028f18
JOSE RICARDO BILCHES GONZALEZ <jbilches@uniguajira.edu.co>
2025-02-02 22:02:21 -0500

    modificado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app =express();
 
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:4200'}));
+app.use(cors({origin:'*'}));
 app.use('/api/usuarios', require('./routes/route.usuario'));
 app.use('/api/empleados', require('./routes/route.empleado'));
 app.listen(3005, ()=>{
